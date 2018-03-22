694dd9c671dfffe961bdc02534d79cd78df7131b
abhijithsampathkrishna <aj.sampathk@gmail.com>
2018-03-23 00:18:48 +0530

    Serving HTMl

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY!')
-})
+// app.get('/', (req, res) => {
+//   res.send('HEY!')
+// })
+app.use(express.static('public'))
+
 app.listen(3020, () => console.log('Server running on port 3020'))
