6d900e0adf0e25b60bdc34ce19b342427df2c5b5
abhijithsampathkrishna <aj.sampathk@gmail.com>
2018-03-23 21:55:45 +0530

    Comm test:2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.static('public'))
 // app.use(bodyParser.json())
 app.post('/comm', function (request, response) {
-  var result = {result: 'Success,recieved ', data: request.body.main}
+  var result = {result: 'Success,recieved ', data: request.body}
   // var responsejson = response.body
   // result = result.concat(responsejson)
   // response.send(request.body)
