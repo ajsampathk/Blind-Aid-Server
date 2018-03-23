c17d38274717f51e0f84a716d7248bb9dfa61926
abhijithsampathkrishna <aj.sampathk@gmail.com>
2018-03-23 19:23:02 +0530

    JSON concat bug fix

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const app = express()
 app.use(bodyParser.json())
 app.post('/', function (request, response) {
   var result = {result: 'Success recieved '}
+  var responsejson = response.body
+  result = result.concat(responsejson)
   // response.send(request.body)
-  response.send(JSON.stringify(result.concat(request.body)))
+  response.send(JSON.stringify(result))
 })
 
 app.listen(3020, () => console.log('Server running on port 3020'))
