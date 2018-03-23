439ca76e6b0f799c8360f38c2ed9471ed3c1fb50
abhijithsampathkrishna <aj.sampathk@gmail.com>
2018-03-23 19:32:34 +0530

    HTML rerun

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(bodyParser.json())
-app.post('/', function (request, response) {
-  var result = {result: 'Success,recieved '}
-  // var responsejson = response.body
-  // result = result.concat(responsejson)
-  // response.send(request.body)
-  response.send(JSON.stringify(result))
-})
+app.use(express.static('public'))
+// app.use(bodyParser.json())
+// app.post('/', function (request, response) {
+//   var result = {result: 'Success,recieved '}
+//   // var responsejson = response.body
+//   // result = result.concat(responsejson)
+//   // response.send(request.body)
+//   response.send(JSON.stringify(result))
+// })
 
 app.listen(3020, () => console.log('Server running on port 3020'))
