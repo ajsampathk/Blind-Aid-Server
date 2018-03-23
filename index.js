3ead36db33005d2b9bc2c06cac82fc393e68a0fa
abhijithsampathkrishna <aj.sampathk@gmail.com>
2018-03-23 21:50:46 +0530

    Added /comm

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const app = express()
 
 app.use(express.static('public'))
 // app.use(bodyParser.json())
-// app.post('/', function (request, response) {
-//   var result = {result: 'Success,recieved '}
-//   // var responsejson = response.body
-//   // result = result.concat(responsejson)
-//   // response.send(request.body)
-//   response.send(JSON.stringify(result))
-// })
+app.post('/comm', function (request, response) {
+  var result = {result: 'Success,recieved ', data: request.body}
+  // var responsejson = response.body
+  // result = result.concat(responsejson)
+  // response.send(request.body)
+  response.send(JSON.stringify(result))
+})
 
 app.listen(3020, () => console.log('Server running on port 3020'))
