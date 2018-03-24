133a1c1f42c719c1b1ff8bb5addac1094b8a6a3a
abhijithsampathkrishna <aj.sampathk@gmail.com>
2018-03-24 11:06:05 +0530

    JSON response completed

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express')
-// const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 app.post('/comm', function (request, response) {
   var result = {result: 'Success,recieved '}
-  var data = JSON.parse(request.query)
-  // var responsejson = response.body
-  // result = result.concat(responsejson)
-  // response.send(request.body)
-  response.send(JSON.stringify(data))
+  result = jsonConcat(result, request.body)
+  console.log(result)
+  response.send(JSON.stringify(result))
 })
 
 app.listen(3020, () => console.log('Server running on port 3020'))
+
+function jsonConcat (object1, object2) {
+  for (var key in object2) {
+    object1[key] = object2[key]
+  }
+  return object1
+}
