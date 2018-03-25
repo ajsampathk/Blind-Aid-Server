3558cfc62a178755ca71a3c2f78611cec8334302
abhijithsampathkrishna <aj.sampathk@gmail.com>
2018-03-25 19:40:07 +0530

    Tested OK: Mongodb connection

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const dbClient = require('mongodb').MongoClient
 
 const app = express()
 
+var url = 'mongodb://localhost:27017/'
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 app.use(bodyParser.json())
-app.post('/comm', function (request, response) {
-  var result = {result: 'Success,recieved '}
-  result = jsonConcat(result, request.body)
-  console.log(result)
-  response.send(JSON.stringify(result))
+
+app.post('/signup', function (request, response) {
+  var result = {result: 'Success'}
+
+  dbClient.connect(url, function (err, db) {
+    if (err) {
+      response.send(JSON.stringify({result: 'Failed'}))
+      throw err
+    } else {
+      console.log('connected to database')
+      global.database = db
+      response.send(JSON.stringify(result))
+    }
+  })
+
+  // result = jsonConcat(result, request.body)
+  // console.log(result)
 })
-
 app.listen(3020, () => console.log('Server running on port 3020'))
-
-function jsonConcat (object1, object2) {
-  for (var key in object2) {
-    object1[key] = object2[key]
-  }
-  return object1
-}
