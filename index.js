e07527817a0eed4fbbaa1e7c907d95d56c93b892
abhijithsampathkrishna <aj.sampathk@gmail.com>
2018-03-26 19:46:24 +0530

    Signup Complete

--- index.js
+++ index.js
@@ -9,21 +9,30 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
-app.post('/signup', function (request, response) {
-  var result = {result: 'Success'}
+app.listen(3020, () => console.log('Server running on port 3020'))
 
+app.post('/signup', function (request, response) {
+  global.req = request
   dbClient.connect(url, function (err, db) {
     if (err) {
       response.send(JSON.stringify({result: 'Failed'}))
       throw err
     } else {
-      console.log('connected to database')
+      // console.log('connected to database')
       global.database = db
-      response.send(JSON.stringify(result))
+      insertUser()
+      response.send(JSON.stringify({result: 'Success'}))
+      db.close()
     }
   })
-
-  // result = jsonConcat(result, request.body)
-  // console.log(result)
 })
-app.listen(3020, () => console.log('Server running on port 3020'))
+
+function insertUser () {
+  var req = global.req
+  var db = global.database.db('Users')
+  var userobject = {username: req.body.username, key: req.body.password}
+  db.collection('Users').insertOne(userobject, function (err, res) {
+    if (err) throw err
+    // console.log('Insert completed')
+  })
+}
