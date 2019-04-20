c70715b37c6d05b7b8b5ab5d4c4640900c441c09
ajsampathk <aj.sampathk@gmail.com>
2019-04-20 16:01:33 +0530

    Refactor for Location Share

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-188-137-2.us-east-2.compute.amazonaws.com',
-      key: '~/Downloads/server_1.pem',
+      host: 'ec2-18-221-46-221.us-east-2.compute.amazonaws.com',
+      key: '~/LocationShare.pem',
       ref: 'origin/master',
       repo: 'git@github.com:abhijithsampathkrishna/Blind-Aid-Server.git',
       path: '/home/ubuntu/server',
