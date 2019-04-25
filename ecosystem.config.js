87c27d10f6efd803fd2b38db1871eeaea37eba27
ajsampathk <aj.sampathk@gmail.com>
2019-04-25 11:16:01 +0530

    Migration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-18-220-159-36.us-east-2.compute.amazonaws.com',
-      key: '~/LocationShareServer.pem',
+      key: '~/Downloads/LocationServer.pem',
       ref: 'origin/master',
       repo: 'git@github.com:abhijithsampathkrishna/Blind-Aid-Server.git',
       path: '/home/ubuntu/server',
